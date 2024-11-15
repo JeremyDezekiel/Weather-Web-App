43b7350b053e87c8bf6a7d9c3e87462129b43fdc
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-11-15 12:08:15 +0700

    use funciton getDataDaily to #tomorrow

--- app.js
+++ app.js
@@ -1,86 +1,50 @@
-// const daily = {
-//     time: [
-//         "2024-10-14",
-//         "2024-10-15",
-//         "2024-10-16",
-//         "2024-10-17",
-//         "2024-10-18",
-//         "2024-10-19",
-//         "2024-10-20",
-//     ],
-//     temperature_2m: [11.5, 11.8, 13.7, 15.5, 16.5, 15.3, 13.5],
-// };
-
-// const days = document.querySelector('#days')
-
-// for (let i = 0; i < daily.time.length; i++) {
-//     // console.log(formattedDate(daily.time[i]))
-//     days.innerHTML += `
-//         <div class="d-flex justify-content-between align-items-center">
-//             <div>
-//                 <span>${formattedDate(daily.time[i])}</span>
-//             </div>
-//             <div>
-//                 <img width="50px" src="asset/water-drop.gif" alt="water-drop">
-//                 <span>56%</span>
-//                 <img width="50px" src="asset/cloudy-day.gif" alt="cloudy-day">
-//                 <img width="50px" src="asset/cloudy-night.gif" alt="cloudy-night">
-//                 <span>${daily.temperature_2m[i]}</span>
-//                 <span>26°</span>
-//             </div>
-//         </div>
-//     `
-// }
-
-// function formattedDate (date) {
-//     const options = {
-//         weekday: 'long',
-//         year : 'numeric',
-//         month : 'long',
-//         day : 'numeric',
-//     }
-//     const newDate = new Date(date)
-//     const formatted = newDate.toLocaleDateString('en-US', options)
-//     return formatted
-// }
-
-// function dailyDateFormated(date) {
-//     const options = {
-//         weekday: 'long',
-//         year: 'numeric',
-//         month: 'long',
-//         day: 'numeric',
-//     };
-//     const dateFormatted = new Date(date);
-//     return dateFormatted.toLocaleDateString('en-US', options);
-// }
-
-// 
-
-const URL = 'https://api.open-meteo.com/v1/forecast?latitude=-7.2492&longitude=112.7508&current=temperature_2m,apparent_temperature,is_day,weather_code&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,visibility&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset'
-// const weekly = document.querySelector('#weekly')
+const URL = 'https://api.open-meteo.com/v1/forecast?latitude=-7.2492&longitude=112.7508&current=temperature_2m,apparent_temperature,is_day,weather_code&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,visibility&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=Asia%2FSingapore'
 
 function showGifWeather(code) {
     if (code == 0) {
-        return '<img src="gif/0"/>'
+        return '<img width="50px" src="gif/0.gif"/>'
     } else if (code == 1) {
-        return '<img src="gif/1"/>'
+        return '<img width="50px" src="gif/1.gif"/>'
     } else if (code == 2) {
-        return '<img src="gif/2"/>'
+        return '<img width="50px" src="gif/2.gif"/>'
     } else if (code == 3) {
-        return '<img src="gif/3"/>'
+        return '<img width="50px" src="gif/3.gif"/>'
     } else if (code == 45) {
-        return '<img src="gif/45"/>'
+        return '<img width="50px" src="gif/45.gif"/>'
     } else if (code == 48) {
-        return '<img src=""/>'
+        return '<img width="50px" src=""/>'
     } else if (code == 51 || code == 53 || code == 55 || code == 80 || code == 81 || code == 82 || code == 61 || code == 63 || code == 65) {
-        return '<img src="gif/51,53,55,80,81,82,61,63,65.gif"/>'
+        return '<img width="50px" src="gif/51,53,55,80,81,82,61,63,65.gif"/>'
     } else if (code == 56 || code == 57 || code == 66 || code == 67) {
-        return '<img src="gif/56,57,66,67.gif"/>'
+        return '<img width="50px" src="gif/56,57,66,67.gif"/>'
     } else if (code == 77 || code == 85 || code == 86 || code == 71 || code == 73 || code == 75) {
-        return '<img src="gif/77,85,86,71,73,75.gif"/>'
+        return '<img width="50px" src="gif/77,85,86,71,73,75.gif"/>'
     } else if (code == 95 || code == 96 || code == 99) {
-        return '<img src="gif/95,96,99.gif"/>'
+        return '<img width="50px" src="gif/95,96,99.gif"/>'
+    }
+}
+
+function showWeatherText(code) {
+    if (code == 0) {
+        return 'Clear'
+    } else if (code == 1) {
+        return 'Mostly Clear'
+    } else if (code == 2) {
+        return 'Partly Clear'
+    } else if (code == 3) {
+        return 'Overcast'
+    } else if (code == 45) {
+        return 'Fog'
+    } else if (code == 48) {
+        return 'Icy Fog'
+    } else if (code == 51 || code == 53 || code == 55 || code == 80 || code == 81 || code == 82 || code == 61 || code == 63 || code == 65) {
+        return 'Rainny'
+    } else if (code == 56 || code == 57 || code == 66 || code == 67) {
+        return 'Snow Rain'
+    } else if (code == 77 || code == 85 || code == 86 || code == 71 || code == 73 || code == 75) {
+        return 'Snow'
+    } else if (code == 95 || code == 96 || code == 99) {
+        return 'Storm'
     }
 }
 
@@ -95,13 +59,25 @@ function dailyDateFormated(date) {
     return dateFormatted.toLocaleDateString('en-US', options)
 }
 
+function hourlyTime(time) {
+    options = {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        timeZone: "Asia/Singapore",
+        timeZoneName: "short",
+    }
+    const timeFormat = new Date(time)
+    return timeFormat.DateTimeFormat('en-US', options)
+}
+
 async function getDataDaily() {
     try {
         const fetchDaily = await fetch(URL)
         const dataDaily = await fetchDaily.json()
 
         dataDaily.daily.weather_code.forEach((element, i) => {
-            weekly.innerHTML += `
+            document.getElementById('weekly').innerHTML += `
                 <div class="d-flex justify-content-between align-items-center">
                     <div>
                         <span>${dailyDateFormated(dataDaily.daily.time[i])}</span>
@@ -109,12 +85,23 @@ async function getDataDaily() {
                     <div>
                     <img width="50px" src="asset/water-drop.gif" alt="water-drop">
                     <span>56%</span>
-                    <img width="50px" src="asset/cloudy-day.gif" alt="cloudy-day">
-                    <img width="50px" src="asset/cloudy-night.gif" alt="cloudy-night">
+                    <span>
+                    ${showGifWeather(element)}
+                    <span>
                     <span>${dataDaily.daily.temperature_2m_max[i]}°C</span>
                     <span>${dataDaily.daily.temperature_2m_min[i]}°C</span>
                     </div>
                 </div>
+            `
+            tomorrow.innerHTML = `
+                    <div class="col-12">
+                        <p class="mb-0 mt-3">Tomorrow</p>
+                        <h3 class="mb-0">Surabaya,Indonesia</h3>
+                    </div>
+                    <div class="col-12 d-flex flex-column justify-content-end">
+                        <h3 class="mb-0">${dataDaily.daily.temperature_2m_min[1]}°C</h3>
+                        <p class="mb-3">${showWeatherText(element)}</p>
+                    </div>
             `
         })
     } catch (error) {
@@ -123,6 +110,18 @@ async function getDataDaily() {
 }
 
 getDataDaily()
+
+async function getDataCurrent() {
+    try {
+        const fetchCurrent = await fetch(URL)
+        const dataCurrent = await fetchCurrent.json()
+
+        console.log(dataCurrent)
+        
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 // async function getDataCurrent() {
 //     try {
